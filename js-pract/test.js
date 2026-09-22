af0d791f35dd1dd793de4be03fdbee6402884f13
muhammadhasan-daffodilsw <muhammad.hasan@daffodilsw.com>
2026-09-22 13:55:55 +0530

    feat:various assignments

--- js-pract/test.js
+++ js-pract/test.js
@@ -5,6 +5,7 @@ const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 async function modifyDoc()
 {
 
+
     for(let i=0;i<5;i++)
     {
     document.getElementById("test").innerText=str[i];
@@ -12,6 +13,36 @@ async function modifyDoc()
     }
 }
 
+
 let externalAlert = ()=>{alert("External Alert")};
 
-let str = ['catch','me','if','you','can'];
+const str = ['catch','me','if','you','can'];
+
+class User
+{
+    constructor(username,email)
+    {
+        this.username=username;
+        this.email=email;
+    }
+
+    getEmail(){
+    
+    }
+}
+
+let user = new User("idk","email");
+console.log(typeof user);
+console.log(typeof str);
+
+function test_text_embedding(){
+alert(`hello ${user.username}`);
+alert('hello ${user.username}');
+}
+
+function test_prompts(){
+    let a= prompt('Enter Two Numbers,spaced').split(' ');
+    a = a.map(Number);
+    alert(a[0]+a[1]);
+
+}
